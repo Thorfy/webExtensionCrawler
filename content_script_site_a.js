a189366aa4fc7218ccf042f77a3a62eb7d4ab6b1
Thorfy <lefebvre.remy.pro@gmail.com>
2023-04-15 18:25:54 +0200

    logic inside script a

--- content_script_site_a.js
+++ content_script_site_a.js
@@ -1,12 +1,41 @@
 function extractDataSiteA(doc) {
     // Logique d'extraction des données pour le site A
-  }
-  
-  function sendDataToBackgroundScript(message) {
+
+    const title = extractTitle(doc);
+    const description = extractDescription(doc);
+    const imageUrls = extractImageUrls(doc);
+
+    return {
+        title,
+        description,
+        imageUrls
+    };
+}
+
+
+function extractTitle(doc) {
+    const titleElement = doc.querySelector("meta[property='og:title']");
+    return titleElement ? titleElement.content : doc.title;
+}
+
+function extractDescription(doc) {
+    const descriptionElement = doc.querySelector("meta[name='description']");
+    return descriptionElement ? descriptionElement.content : "";
+}
+
+function extractImageUrls(doc) {
+    const imageElements = doc.querySelectorAll("img");
+    const imageUrls = Array.from(imageElements).map(img => img.src);
+    return imageUrls;
+}
+
+
+
+function sendDataToBackgroundScript(message) {
     chrome.runtime.sendMessage(message);
-  }
-  
-  sendDataToBackgroundScript({
+}
+
+sendDataToBackgroundScript({
     site: "SiteA",
     data: extractDataSiteA(document)
-  });
+});
